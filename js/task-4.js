2a5a6709934f710bbe376214d2d693a1bd6df693
Khrystyna Storozhuk <khrystynastorozhuk72@gmail.com>
2024-09-27 14:40:56 -0400

    add script

---
+++ js/task-4.js
@@ -0,0 +1,18 @@
+const formSubmit = document.querySelector('form');
+console.log(formSubmit);
+formSubmit.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === '' || password === '') {
+    return alert('All form fields must be filled in');
+  }
+  console.log({
+    email: { name: 'email', value: email },
+    password: { name: 'password', value: password },
+  });
+  form.reset();
+}
