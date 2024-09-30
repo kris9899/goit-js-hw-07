b2e4ca551a6422a15c31d6c1a3292410d3c7dcbb
Khrystyna Storozhuk <khrystynastorozhuk72@gmail.com>
2024-09-30 16:34:26 -0400

    add script

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const elementCountInput = document.getElementById('elementCount');
+const boxesContainer = document.getElementById('boxes');
+const INITIAL_SIZE = 30;
+
+createButton.addEventListener('click', () => {
+  const input = document.querySelector('#controls input');
+  const numberOfBoxes = parseInt(input.value);
+
+  if (numberOfBoxes > 0 && numberOfBoxes <= 100) {
+    boxesContainer.innerHTML = '';
+    const boxes = Array.from({ length: numberOfBoxes }, (_, i) => {
+      const size = INITIAL_SIZE + i * 10;
+      return `<div style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};"></div>`;
+    }).join('');
+
+    boxesContainer.innerHTML = boxes;
+  }
+
+  if (input.value !== '') {
+    input.value = '';
+  }
+});
+
+destroyButton.addEventListener('click', () => {
+  boxesContainer.innerHTML = '';
+});
